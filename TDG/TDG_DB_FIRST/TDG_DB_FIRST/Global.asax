95fe1d6676c8937abcccccee1592c40ef740a45e
Diego Maŕquez <desarrollo.marquez@gmail.com>
2019-02-04 23:35:31 -0500

    Adicion app web  MVC .NET

---
+++ TDG/TDG_DB_FIRST/TDG_DB_FIRST/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TDG_DB_FIRST.MvcApplication" Language="C#" %>
---
+++ TDG/TDG_DB_FIRST/TDG_DB_FIRST/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TDG_DB_FIRST.Startup))]
+namespace TDG_DB_FIRST
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
