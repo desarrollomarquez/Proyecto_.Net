9e179a19d6f6175eb954f7ed4b5eae9500f4eed8
Diego Maŕquez <desarrollo.marquez@gmail.com>
2019-02-11 22:43:15 -0500

    Adicion modelo ADO web  MVC .NET

---
+++ TDG/TDG_DB_FIRST/TDG_DB_FIRST/Controllers/TipoVehiculoController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TDG_DB_FIRST.Controllers
+{
+    public class TipoVehiculoController : Controller
+    {
+        // GET: TipoVehiculo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: TipoVehiculo/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TipoVehiculo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TipoVehiculo/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TipoVehiculo/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TipoVehiculo/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TipoVehiculo/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TipoVehiculo/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
